3a4589b0681f782eb581116cbcf5e33beadb2007
Pietro Giordano <giopi87@gmail.com>
2023-05-31 14:41:59 +0200

    FizzBuzz compreso di bonus // dettaglio da aggiungere

---
+++ src/org/lessons/java/FizzBuzz.java
@@ -0,0 +1,29 @@
+package org.lessons.java;
+
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        // Scanner
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Inserisci un numero compreso tra 1 e 1000");
+        int userInput = scanner.nextInt();
+
+        if (userInput < 1 || userInput > 1000) {
+            System.out.println("Numero non valido");
+        } else {
+            for (int i = 1; i <= userInput; i++) {
+                if (i % 3 == 0 && i % 5 == 0) {
+                    System.out.print("FizzBuzz ");
+                } else if (i % 3 == 0) {
+                    System.out.print("Fizz ");
+                } else if (i % 5 == 0) {
+                    System.out.print("Buzz ");
+                } else {
+                    System.out.print(i + " ");
+                }
+            }
+        }
+    }
+}
